3c813b073f30fee38fa9866d9dea7a6fb3690d1a
Aime Malaika <aime@localhost.localdomain>
2017-11-06 07:24:47 -0600

    adding editor api

--- sendmessage.php
+++ sendmessage.php
@@ -1,13 +1,23 @@
+<?php
+    
+    session_start();
+    if (!isset($_SESSION['fisrtname']) AND !isset($_SESSION['emailaddress'])) {
+      header('Location: index.php');
+    }
+    //include 'php/publish.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
   <meta charset="utf-8">
+  
   <title>mailing panel</title>
+ <script src="//cdn.ckeditor.com/4.7.3/full/ckeditor.js"></script>
 </head>
 
-<body>     
- <form method="POST" action="php/publish.php">
+<body>
+<form method="POST" action="php/publish.php">
             <div class="form-group">
               <label for="usr">Subject of your message:</label>
               <input type="text" name="sujet" class="form-control" id="usr">
@@ -22,8 +32,7 @@
                 } );
             </script><br>
             <center><button name="submit" style="width: 50%;background-color: #343a40" type="submit" class="btn btn-primary">Diffuse message to suscribers</button></center>
-</form> 
-
+          </form> 
 </body>
 
 </html>
